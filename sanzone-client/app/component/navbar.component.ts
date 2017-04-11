5c5532de1a4b4588962d5e718d3c6b0093619531
Dmitry.Haiduk <dementeyyy@gmail.com>
2017-04-11 16:57:32 +0300

    Created a header (navbar)

--- sanzone-client/app/component/navbar.component.ts
+++ sanzone-client/app/component/navbar.component.ts
@@ -7,13 +7,41 @@ import {Component} from '@angular/core';
     template: `
                 <nav class="navbar navbar-default">
                     <div class="container-fluid">
-                        <div class="navbar-header">
-                            <a class="navbar-brand" href="#">
-                                <img alt="Brand" src="./logo_EMFs.png">
-                            </a>
-                        </div>
+                         <div class="navbar-header">
+                              <a class="navbar-brand" href="#">Electro Marnetic Field Software
+                              <img class="logo-emfs" alt="Brand" src="http://res.cloudinary.com/emfs/image/upload/v1491570457/logo-emfs_h4hrpj.svg">
+                              </a>
+                              <button type="button" class="btn btn-default navbar-btn pull-right">Sign in</button>
+                         </div>
                     </div>
-                </nav>`
-} )
+                </nav>
+              `,
+
+
+    styles: [
+             `
+             .navbar
+                {
+                 background-color:transparent;
+                }
+                
+             .logo-emfs
+                {
+                 position: absolute;
+	             top: 0;
+	             bottom: 0;
+	             left: 0;
+	             right: 0;
+	             height: 80%;
+	             margin: auto;
+                }
+                
+             .btn
+                {
+                 float: right;
+                }                
+             `
+            ]
+         } )
 
 export class NavbarComponent { }
