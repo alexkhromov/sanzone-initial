b7fb37d1961c7af5850810b7002e708c45376223
Dmitry.Haiduk <dementeyyy@gmail.com>
2017-05-23 16:58:13 +0300

    Created a header (navbar)

--- sanzone-client/app/component/navbar.component.ts
+++ sanzone-client/app/component/navbar.component.ts
@@ -4,10 +4,8 @@ import {Component} from '@angular/core';
 
     selector: 'navbar',
 
-    template: `
-                <nav class="navbar navbar-default ">
+    template: `<nav class="navbar navbar-default navbar-fixed-top">
                     <div class="container-fluid">
-                         <!-- Brand and toggle get grouped for better mobile display -->
                               <div class="navbar-header">
                                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                                   <span class="sr-only">Toggle navigation</span>
@@ -15,40 +13,72 @@ import {Component} from '@angular/core';
                                       <span class="icon-bar"></span>
                                       <span class="icon-bar"></span>
                                   </button>
-                                  <a class="navbar-brand" href="#">Electro Magnetic Field Software
-                                  <img class="logo-emfs" alt="logo-emfs" src="http://res.cloudinary.com/emfs/image/upload/v1491570457/logo-emfs_h4hrpj.svg">
-                                  </a>
+                              </div> 
+                              
+                              <div class="brand-centered"> 
+                                   <div class="navbar-brand" href="#">
+                                   <img src="http://res.cloudinary.com/emfs/image/upload/v1491570457/logo-emfs_h4hrpj.svg" alt="logo">
+                                   
+                                   </div>
                               </div>
+                              
+                              <button type="button" class="btn btn-primary">login</button>
                     </div>
                 </nav>
+                
+           
               `,
 
 
     styles: [
              `
-             .navbar
-                {
-                 background-color:green;
-                }
-             .navbar-brand
-                {
-                 color: blue;
-                }          
-             .logo-emfs
-                {
-                 position: absolute;
-	             top: 0;
-	             bottom: 0;
-	             left: 0;
-	             right: 0;
-	             height: 80%;
-	             margin: auto;
-                }
-                
-             .btn
-                {
+             .navbar {
+             background-color: #00BFFF; 
+             }
+             
+             h5 {
+             color: #FFFFFF;
+             }
+             
+             .img{
+             margin-right: 10px;
+             padding: 0;
+             }
+             
+             .btn {
                  float: right;
-                }                
+                 
+             }  
+                   
+             .navbar-brand>img {
+                 height: 40px;
+                 /*padding: 0px;*/
+                 width: auto;
+             }
+             
+             .brand-centered {
+             display: flex;
+             justify-content: center;
+             position: absolute;
+             width: 100%;
+             left: 0;
+             top: 0;
+             }
+             
+             .brand-centered .navbar-brand  {
+             display: flex;
+             align-items: center;
+             }
+             .navbar-toggle {
+             z-index: 1;
+             }
+   
+                              
+                              
+                              
+                              
+                              
+                              
              `
             ]
          } )
