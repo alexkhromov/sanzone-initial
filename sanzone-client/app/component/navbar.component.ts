dcbc8d7f882a543477f1ea8f47a4fccc71a6f5e5
Dmitry.Haiduk <dementeyyy@gmail.com>
2017-05-24 16:50:31 +0300

    Created a header (navbar)

--- sanzone-client/app/component/navbar.component.ts
+++ sanzone-client/app/component/navbar.component.ts
@@ -4,27 +4,62 @@ import {Component} from '@angular/core';
 
     selector: 'navbar',
 
-    template: `<nav class="navbar navbar-default navbar-fixed-top">
-                    <div class="container-fluid">
-                              <div class="navbar-header">
-                                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                  <span class="sr-only">Toggle navigation</span>
-                                      <span class="icon-bar"></span>
-                                      <span class="icon-bar"></span>
-                                      <span class="icon-bar"></span>
-                                  </button>
-                              </div> 
-                              
-                              <div class="brand-centered"> 
-                                   <div class="navbar-brand" href="#">
-                                   <img src="http://res.cloudinary.com/emfs/image/upload/v1491570457/logo-emfs_h4hrpj.svg" alt="logo">
-                                   
-                                   </div>
-                              </div>
-                              
-                              <button type="button" class="btn btn-primary">login</button>
-                    </div>
-                </nav>
+    template: `<nav class="navbar navbar-default">
+                 <div class="container-fluid">
+                   <!-- Brand and toggle get grouped for better mobile display -->
+                   <div class="navbar-header">
+                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                       <span class="sr-only">Toggle navigation</span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                         <span class="icon-bar"></span>
+                     </button>
+                     <a class="navbar-brand" href="#">
+                       <img src="http://res.cloudinary.com/emfs/image/upload/v1491570457/logo-emfs_h4hrpj.svg" class="img-responsive" alt="Responsive image">
+                     </a>
+                   </div>
+                     
+                   <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+                     
+                 </div> 
+               </nav>
                 
            
               `,
@@ -32,53 +67,13 @@ import {Component} from '@angular/core';
 
     styles: [
              `
-             .navbar {
-             background-color: #00BFFF; 
-             }
-             
-             h5 {
-             color: #FFFFFF;
-             }
-             
-             .img{
-             margin-right: 10px;
-             padding: 0;
-             }
-             
-             .btn {
-                 float: right;
-                 
-             }  
-                   
+                
              .navbar-brand>img {
-                 height: 40px;
-                 /*padding: 0px;*/
-                 width: auto;
+             height: 100%;
+             padding: 0px;
+             width: auto;
              }
              
-             .brand-centered {
-             display: flex;
-             justify-content: center;
-             position: absolute;
-             width: 100%;
-             left: 0;
-             top: 0;
-             }
-             
-             .brand-centered .navbar-brand  {
-             display: flex;
-             align-items: center;
-             }
-             .navbar-toggle {
-             z-index: 1;
-             }
-   
-                              
-                              
-                              
-                              
-                              
-                              
              `
             ]
          } )
