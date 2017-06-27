35c1569be82acf539042460a9c148257487a96f2
Dmitry.Haiduk <dementeyyy@gmail.com>
2017-06-27 16:51:10 +0300

    Application of post.servis for showing of example calculation

--- sanzone-client/app/component/app.module.ts
+++ sanzone-client/app/component/app.module.ts
@@ -7,6 +7,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from "./app.component";
 import {NavbarComponent} from "./navbar.component";
 import {ExampleComponent} from "./example.component";
+import {PostComponent} from "./arhiv/post.component";
+import {TwitComponent} from "./arhiv/twit.component";
+import {HeartComponent} from "./arhiv/heart.component";
+import {PostService} from "../service/post.service";
 
 
 @NgModule( {
@@ -17,9 +21,15 @@ import {ExampleComponent} from "./example.component";
 
         AppComponent,
         NavbarComponent,
-        ExampleComponent
+        ExampleComponent,
+        TwitComponent,
+        PostComponent,
+        HeartComponent
     ],
 
+    providers: [
+        PostService
+    ],
 
     bootstrap: [
         AppComponent
--- sanzone-client/app/component/example.component.ts
+++ sanzone-client/app/component/example.component.ts
@@ -8,13 +8,13 @@ export class Example {
 }
 
 const EXAMPLES: Example[] = [
-    { id: 1, name: 'Расчет санитарно-защитной зоны в горизонтальной плоскости антенн расположенных в одной точке пространства при одинаковых характеристиках антенн',article:'Данный расчет позволяет рассчитывать санитарно-защитную зону при расположении антенн в одной точке в горизонтальной плоскости, т.е. при совпадении координат (x, y, z) антенн.',       imagePath: 'http://res.cloudinary.com/emfs/image/upload/v1497878339/d4bb0131-0bea-4f42-8e66-1bf3d14ff665_sanzone_H_hbeem2.jpg' },
-    { id: 2, name: 'Расчет санитарно-защитной зоны в горизонтальной плоскости антенн расположенных в одной точке пространства при одинаковых характеристиках антенн', article:'', imagePath: 'http://res.cloudinary.com/emfs/image/upload/v1497878339/df2bce65-c2be-4a17-9d03-9beb5bda1fac_sanzone_H_gizvin.jpg' },
-    { id: 3, name: 'Расчет в горизонтальной плоскости 3', imagePath: 'http://res.cloudinary.com/emfs/image/upload/v1497878339/ca898906-3643-4cc2-9d68-43c7db7194a7_sanzone_H_cqeedc.jpg' },
-    { id: 4, name: 'Расчет в горизонтальной плоскости 4', imagePath: 'http://res.cloudinary.com/emfs/image/upload/v1497878339/4a231c7a-00ec-46bf-a9d8-3c7db3c50348_sanzone_H_cj4xld.jpg' },
-    { id: 5, name: 'Расчет в горизонтальной плоскости 5', imagePath: 'http://res.cloudinary.com/emfs/image/upload/v1497878339/8145a307-f9dc-4528-a8f9-e4df79c0fdab_sanzone_H_ugadio.jpg' },
-    { id: 6, name: 'Расчет в горизонтальной плоскости 6', imagePath: 'http://res.cloudinary.com/emfs/image/upload/v1497878339/1c534d8a-da32-4ce8-b466-aed1622b3776_sanzone_H_t9wlxz.jpg' },
-    { id: 7, name: 'Расчет в вертикальной плоскости', imagePath: 'http://res.cloudinary.com/emfs/image/upload/v1497878339/f5bb36c9-1213-4259-b3de-56776c47a7a2_sanzone_V_ppm2zh.jpg' },
+    { id: 1, name: 'Расчет санитарно-защитной зоны в горизонтальной плоскости антенн расположенных в одной точке пространства при одинаковых характеристиках антенн',article:'Данный расчет позволяет рассчитывать санитарно-защитную зону при расположении антенн в одной точке в горизонтальной плоскости, т.е. при совпадении координат (x, y, z) антенн.',       imagePath: 'http://res.cloudinary.com/sanzone/image/upload/v1498126296/1_kexgrw.jpg' },
+    { id: 2, name: 'Расчет санитарно-защитной зоны в горизонтальной плоскости антенн расположенных в одной точке пространства при одинаковых характеристиках антенн', article:'', imagePath: 'http://res.cloudinary.com/sanzone/image/upload/v1498126296/2_rdpf7i.jpg' },
+    { id: 3, name: 'Расчет в горизонтальной плоскости 3', imagePath: 'http://res.cloudinary.com/sanzone/image/upload/v1498126296/3_dpcjyw.jpg' },
+    { id: 4, name: 'Расчет в горизонтальной плоскости 4', imagePath: 'http://res.cloudinary.com/sanzone/image/upload/v1498126296/4_eeklzm.jpg' },
+    { id: 5, name: 'Расчет в горизонтальной плоскости 5', imagePath: 'http://res.cloudinary.com/sanzone/image/upload/v1498126296/5_csy2yi.jpg' },
+    { id: 6, name: 'Расчет в горизонтальной плоскости 6', imagePath: 'http://res.cloudinary.com/sanzone/image/upload/v1498126296/6_z9sgpp.jpg' },
+    { id: 7, name: 'Расчет в вертикальной плоскости', imagePath: 'http://res.cloudinary.com/sanzone/image/upload/v1498126296/7_e2lv6t.jpg' },
 ];
 
 @Component({
