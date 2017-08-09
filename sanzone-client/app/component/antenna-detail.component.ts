031dcc5c22c68235df98553247a8913c74463572
Alexander.Khromov <hrom1981@gmail.com>
2017-08-09 13:18:07 +0300

    Example of coding style.

--- sanzone-client/app/component/antenna-detail.component.ts
+++ sanzone-client/app/component/antenna-detail.component.ts
@@ -1,18 +1,27 @@
 import { Component, Input } from '@angular/core';
-import { Hero } from './antenna';
-@Component({
+import { Antenna } from './antenna';
+
+@Component( {
+
     selector: 'antenna-detail',
-    template: `
-    <div *ngIf="antenna">
-      <h2>{{antenna.name}} details!</h2>
-      <div><label>id: </label>{{antenna.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="antenna.name" placeholder="name"/>
-      </div>
-    </div>
-  `
-})
+
+    template:
+        `<div *ngIf="antenna">
+            
+            <h2>{{antenna.name}} details!</h2>
+            
+            <div>
+                <label>id: </label>{{antenna.id}}
+            </div>
+            
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="antenna.name" placeholder="name"/>
+            </div>
+        </div>`
+} )
+
 export class AntennaDetailComponent {
+
     @Input() antenna: Antenna;
 }
