90a3146df5eda2a1c2e9e98a0b5f8c00711926cf
Dmitry.Haiduk <dementeyyy@gmail.com>
2017-08-16 22:52:27 +0300

    Create an input form Angular routing1

--- sanzone-client/app/component/antennas.component.ts
+++ sanzone-client/app/component/antennas.component.ts
@@ -11,65 +11,69 @@ import { AntennaService } from './antenna.service';
     selector: 'my-antennas',
 
     template: `
-<div class="container">
+        <div class="container">
 
- <h1>{{title}}</h1>
-    <h2>My Heroes</h2>
-    <ul class="antennas">
-      <li *ngFor="let antenna of antennas"
-        [class.selected]="antenna === selectedAntenna"
-        (click)="onSelect(antenna)">
-        <span class="badge">{{antenna.id}}</span> {{antenna.name}}
-      </li>
-    </ul>
+            <h1>{{title}}</h1>
+            
+            <h2>My Heroes</h2>
+            
+            <ul class="antennas">
+                <li *ngFor="let antenna of antennas"
+                    [class.selected]="antenna === selectedAntenna"
+                    (click)="onSelect(antenna)">
+                    <span class="badge">{{antenna.id}}</span> {{antenna.name}}
+                </li>
+            </ul>
+             
             <antenna-detail [antenna]="selectedAntenna"></antenna-detail>
+                
         </div>
 
 
+        <div class="container">
+        
+            <div class="panel panel-primary">
+ 
+                <!-- Default panel contents -->
+                <div class="panel-heading">Введите параметры радиопередающих устройств</div>
 
-<div class="container">
-<div class="panel panel-primary">
-  <!-- Default panel contents -->
-  <div class="panel-heading">Введите параметры радиопередающих устройств</div>
-
-  <!-- Table -->
-<table class="table">
-  <thead>
-    <tr>
-      <th>#</th>
-              <th>Latitude</th>
-              <th >Longitude</th>
-              <th>Azimuth</th>
-              <th>Down Tilt</th>
-              <th>Height</th>
-              <th>Power</th>
-              <th>Gain</th>
-              <th>Tract Lost</th>
-              <th>Earth Factor</th>
-              <th>Diagram WidthHalf PowerH</th>
-              <th>Diagram WidthHalf PowerV</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <th><input size="5" [(ngModel)]="Latitude" placeholder="Latitude"></th>
-      <th><input size="5" [(ngModel)]="Longitude" placeholder="Longitude"></th>
-      <th><input size="5" [(ngModel)]="Azimuth" placeholder="Azimuth"></th>
-      <th><input size="5" [(ngModel)]="DownTilt" placeholder="DownTilt"></th>
-      <th><input size="5" [(ngModel)]="Height" placeholder="Height"></th>
-      <th><input size="5" [(ngModel)]="Power" placeholder="Power"></th>
-      <th><input size="5" [(ngModel)]="Gain" placeholder="Gain"></th>
-      <th><input size="5" [(ngModel)]="TractLost" placeholder="TractLost"></th>
-      <th><input size="5" [(ngModel)]="EarthFactor" placeholder="EarthFactor"></th>
-      <th><input size="5" [(ngModel)]="DiagramWidthHalfPowerH" placeholder="DiagramWidthHalfPowerH"></th>
-      <th><input size="5" [(ngModel)]="DiagramWidthHalfPowerV" placeholder="DiagramWidthHalfPowerV"></th> 
-    </tr>
-  </tbody>
-</table>
-</div>
-
-              </div>
+                    <!-- Table -->
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Latitude</th>
+                                <th >Longitude</th>
+                                <th>Azimuth</th>
+                                <th>Down Tilt</th>
+                                <th>Height</th>
+                                <th>Power</th>
+                                <th>Gain</th>
+                                <th>Tract Lost</th>
+                                <th>Earth Factor</th>
+                                <th>Diagram WidthHalf PowerH</th>
+                                <th>Diagram WidthHalf PowerV</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <th><input size="5" [(ngModel)]="Latitude" placeholder="Latitude"></th>
+                            <th><input size="5" [(ngModel)]="Longitude" placeholder="Longitude"></th>
+                            <th><input size="5" [(ngModel)]="Azimuth" placeholder="Azimuth"></th>
+                            <th><input size="5" [(ngModel)]="DownTilt" placeholder="DownTilt"></th>
+                            <th><input size="5" [(ngModel)]="Height" placeholder="Height"></th>
+                            <th><input size="5" [(ngModel)]="Power" placeholder="Power"></th>
+                            <th><input size="5" [(ngModel)]="Gain" placeholder="Gain"></th>
+                            <th><input size="5" [(ngModel)]="TractLost" placeholder="TractLost"></th>
+                            <th><input size="5" [(ngModel)]="EarthFactor" placeholder="EarthFactor"></th>
+                            <th><input size="5" [(ngModel)]="DiagramWidthHalfPowerH" placeholder="DiagramWidthHalfPowerH"></th>
+                            <th><input size="5" [(ngModel)]="DiagramWidthHalfPowerV" placeholder="DiagramWidthHalfPowerV"></th> 
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+        </div>
 `,
     styles: [`
     .selected {
