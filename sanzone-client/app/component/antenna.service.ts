4419ed6d738ded7ec452fa8f2b6d7965d5167696
Dmitry.Haiduk <dementeyyy@gmail.com>
2017-10-05 16:42:00 +0300

    Create HTTP calls to a remote server's web API

--- sanzone-client/app/component/antenna.service.ts
+++ sanzone-client/app/component/antenna.service.ts
@@ -22,13 +22,27 @@ import { Antenna } from './antenna';
 
 
     getAntenna(id: number): Promise<Antenna> {
-        return this.getAntennas()
-            .then(antennas => antennas.find(antenna => antenna.id === id));
+        const url = `${this.antennasUrl}/${id}`;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Antenna)
+            .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
+    }
+
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    update(antenna: Antenna): Promise<Antenna> {
+        const url = `${this.antennasUrl}/${antenna.id}`;
+        return this.http
+            .put(url, JSON.stringify(antenna), {headers: this.headers})
+            .toPromise()
+            .then(() => antenna)
+            .catch(this.handleError);
     }
 
     }
