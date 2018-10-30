28ee3cafe8704528be0f8c39771f139ece9c9bd0
Dmitry.Haiduk <dementeyyy@gmail.com>
2018-10-30 22:36:45 +0300

    2018-10-30

--- sanzone-client/src/app/app.module.ts
+++ sanzone-client/src/app/app.module.ts
@@ -1,6 +1,3 @@
-/**
- * Created by DEV on 21.03.2017.
- */
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
